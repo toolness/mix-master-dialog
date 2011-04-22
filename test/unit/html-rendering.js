5dd5a7be196d5f9e125e3b852410fe8c3e144fb7
Atul Varma <varmaa@gmail.com>
2011-04-22 15:19:21 -0700

    Removed not-yet-functional tests.  They will be back.

--- test/unit/html-rendering.js
+++ test/unit/html-rendering.js
@@ -10,22 +10,3 @@ test("setHTML actually renders HTML", function () {
 
   htmlRendering.destroy();
 });
-
-test("specified baseURL is used for relatives", function() {
-  var div = $("#test-rendering");
-  var r = new jQuery.HTMLRendering(div, "http://mozilla.org/foo/");
-  r.setHTML('<a href=".">hi</a>');
-  equals(div.find("a").get(0).href, "http://mozilla.org/foo/");
-  r.destroy();
-});
-
-test("unspecified baseURL uses dialog's base URL for relatives", function() {
-  equals($("base").length, 0);
-  
-  var div = $("#test-rendering");
-  var r = new jQuery.HTMLRendering(div);
-  r.setHTML('<a href=".">hi</a>');
-  equals(div.find("a").get(0).href, window.location.href);
-  r.destroy();
-});
-
